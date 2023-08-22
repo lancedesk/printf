556b28acaabb0643eec7168bdf541691d21ee82b
Lance Desk <lncedesk.com>
2023-08-21 19:03:57 -0700

    Custom C Programming Language Print F function clone

--- _format_c.c
+++ _format_c.c
@@ -2,18 +2,29 @@
 #include "format_helpers.h"
 
 /**
- *_format_c - Handle the %c format specifier
+ * _format_c - Handle the %c format specifier
  * @args: Character to be printed
  *
  * Return: The number of characters printed
  */
-
 int _format_c(va_list args)
 {
-	char c = va_arg(args, int);
+    char c = va_arg(args, int);
 
-	_putchar(c);
-
-	/* Chars like '%c' always print one character */
-	return (1);
+    /* Handle special and unprintable characters */
+    if (c < 32 || c > 126)
+    {
+        /* Print the character as an escape sequence */
+        _putchar('\\');
+        _putchar('x');
+        _putchar((c / 16) + '0');
+        _putchar((c % 16) + ((c % 16) < 10 ? '0' : 'a' - 10));
+        return (4);
+    }
+    else
+    {
+        _putchar(c);
+        return (1);
+    }
 }
+
